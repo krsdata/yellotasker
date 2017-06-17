fc20208d3c2ee948e71271c7d844f8db03b21d16
krsdata <krsdata.net@gmail.com>
2017-06-17 18:23:56 +0530

    changes

--- modules/Admin/views/layouts/header.blade.php
+++ modules/Admin/views/layouts/header.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ $page_title or 'Admin|dashboard' }}</title>
+    <title>Yellotasker CMS</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta http-equiv="refresh" content="900;url={{URL::to('logout')}}" />
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
