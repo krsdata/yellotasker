3d9d9191a39f2efa3294de5460d80fb6658cd2ff
krsdata <krsdata.net@gmail.com>
2017-08-20 14:22:14 +0530

    Encryption

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -10,7 +10,7 @@ class EncryptCookies extends BaseEncrypter
      * The names of the cookies that should not be encrypted.
      *
      * @var array
-     */
+     */ 
     protected $except = [
         //
     ];
