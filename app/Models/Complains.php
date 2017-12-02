3bb5828849f952ffcea9e8a3c3b3c5afefe36981
Nitin Shendkar <Nitin.Shendkar@MOTIFWORKS.IN>
2017-12-03 00:39:07 +0530

    compaint listing view

---
+++ app/Models/Complains.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model as Eloquent; 
+
+class Complains extends Eloquent {
+
+   
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'complains';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+     /**
+     * The primary key used by the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    //protected $dates = ['due_date'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token'
+    ];
+
+    protected $guarded = ['created_at' , 'updated_at' , 'id' ];
+    
+    public  function userDetail()
+    {
+        return $this->hasOne('App\User','id','postedUserId') ;
+    }
+    
+    public  function reportedUserDetail()
+    {
+        return $this->hasOne('App\User','id','reportedUserId') ;
+    }
+
+    public  function taskDetail()
+    {
+        return $this->hasOne('App\Models\Tasks','id','taskId');
+    }
+
+    
+}
