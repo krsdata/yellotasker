880d3e1aedebecb260cefcdc1c525a5bd1460df3
root <kroy.iips@gmail.com>
2017-12-10 12:35:02 -0500

    api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With, auth-token');
 header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Origin: *");
 
 Route::get('/', function () {
 
