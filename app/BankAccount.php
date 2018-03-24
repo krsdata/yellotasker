d6a5599dee0a920192d57a7010365ab8386282a8
Naru Keer <naru.keer@motifworks.com>
2018-03-24 13:15:19 +0530

    bank account naming convention fix

--- app/BankAccount.php
+++ app/BankAccount.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class BankAccount extends Model
 {    protected $primaryKey = 'id';
 
-    protected $table = 'bankaccounts';
+    protected $table = 'bank_accounts';
     //
 }
