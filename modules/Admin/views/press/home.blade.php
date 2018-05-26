a1eb4ecb1bbc5e245810b0f0c2ad0fc4bb495278
programmerlab <kroy.iips@gmail.com>
2018-05-26 17:01:44 +0530

    route issue

--- modules/Admin/views/press/home.blade.php
+++ modules/Admin/views/press/home.blade.php
@@ -75,10 +75,10 @@
                                             <tr>
                                                 <td> {{++$key}} </td>
                                                 <td> {{$result->pressName}} </td>
-                                                <td style="max-width: 300px; word-wrap:break-word">  
+                                                <td style="max-width: 400px; word-wrap:break-word">  
                                                      <a href="{{$result->link}}" target="_blank"> {{ $result->link }}  </a>
                                                 </td>
-                                                <td style="max-width: 300px"> 
+                                                <td style="max-width: 400px"> 
                                                     <?php if(empty($result->articleDescription))
                                                     $result->articleDescription = "NA";
                                                     ?>
