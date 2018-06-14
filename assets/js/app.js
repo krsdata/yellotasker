82b906dadec00026bee5a9bd56fc486d57de2459
Sameer Jain <sameer5@gmail.com>
2018-06-15 01:03:43 +0530

    Payment module

---
+++ assets/js/app.js
@@ -0,0 +1,167 @@
+var app = angular.module('paymentApp', [], function($interpolateProvider) {
+	$interpolateProvider.startSymbol('<%');
+	$interpolateProvider.endSymbol('%>');
+});
+
+
+app.controller('paymentController', function($scope, $http) {
+
+	$scope.todos = [];
+	$scope.loading = false;
+	$scope.userProfit=0;
+	$scope.userNetOutgoing=0;
+	$scope.userNetIncoming=0;
+	$scope.date='';
+	$scope.endDate='';
+	$scope.yelloEarn='';
+	$scope.yelloSpend='';
+	$scope.yelloProfit='';
+	$scope.taskList=[];
+	$scope.userData='';
+	$scope.showList=false;
+
+	$scope.init = function() {
+		$scope.loading = true;
+		$http.get('http://api.yellotasker.com/api/v1/getPostTask?releasedFund=0&taskStatus=completed').
+		success(function(data, status, headers, config) {
+			$scope.todos = data.data;
+      console.log('data 2',$scope.todos);
+				$scope.loading = false;
+
+		});
+	}
+
+
+
+	$scope.releaseFund = function(taskId,userId) {
+		$scope.loading = true;
+		$http.get('http://api.yellotasker.com/api/v1/user/task/release-fund?taskId='+taskId+'&userId='+userId).
+		success(function(data, status, headers, config) {
+			if(data.message=='Task Payment done succesfully.') {
+						var index = $scope.todos.findIndex(x => x.id==taskId);
+						if (index > -1) {
+							$scope.todos.splice(index, 1);
+					}
+					alert('Fund released Successfully')
+				} else {
+					alert('Fund already released')
+				}
+				$scope.loading = false;
+		});
+	}
+
+	$scope.getUserData = function() {
+		var userId=$scope.userId;
+		$scope.loading = true;
+		$http.get('http://api.yellotasker.com/api/v1/user/payments-histroy/outgoing?userId='+userId+'page_size=20&page_num=1').
+		success(function(data, status, headers, config) {
+			if(data.message=='Payment histroy found.') {
+				  $scope.userData=data;
+					$scope.userProfit=data.net_outgoing-data.net_incoming;
+					$scope.userNetOutgoing=data.net_outgoing;
+					$scope.userNetIncoming=data.net_incoming;
+					$scope.taskList=$scope.userData.data.outgoing;
+					$scope.showList=$scope.taskList.length>0?true:false;
+				} else {
+					$scope.showList=false;
+					$scope.userData='';
+					$scope.userProfit=0;
+					$scope.userNetOutgoing=0;
+					$scope.userNetIncoming=0;
+					$scope.taskList=[];
+					alert('No user found with this id');
+				}
+				$scope.loading = false;
+		});
+	}
+	$scope.showTaskList = function(listType){
+		if(listType=='outgoing') {
+      $scope.taskList=$scope.userData.data.outgoing;
+			$scope.showList=$scope.taskList.length>0?true:false;
+		} else if(listType=='incoming') {
+      $scope.taskList=[];
+			$scope.showList=false;
+		}
+	}
+
+	$scope.getYellotaskerData= function() {
+	$scope.loading = true;
+  var startDate=$("#startDate").val();
+	var endDate=$("#endDate").val()
+	$http.get('http://api.yellotasker.com/api/v1/incomeDetail?startDate='+startDate+'&endDate='+endDate).
+	success(function(data, status, headers, config) {
+		if(data.message=='Yellotasker income details') {
+				$scope.yelloEarn=data.data.earn;
+				$scope.yelloSpend=data.data.spend;
+				$scope.yelloProfit=data.data.profit;
+			} else {
+				alert('No details found');
+			}
+			$scope.loading = false;
+	});
+};
+
+	$scope.addTodo = function() {
+		console.log('here');
+				$scope.loading = true;
+
+		$http.post('/api/todos', {
+			title: $scope.todo.title,
+			done: $scope.todo.done
+		}).success(function(data, status, headers, config) {
+			$scope.todos.push(data);
+			$scope.todo = '';
+				$scope.loading = false;
+
+		});
+	};
+
+	$scope.updateTodo = function(todo) {
+		$scope.loading = true;
+
+		$http.put('/api/todos/' + todo.id, {
+			title: todo.title,
+			done: todo.done
+		}).success(function(data, status, headers, config) {
+			todo = data;
+				$scope.loading = false;
+
+		});;
+	};
+
+	$scope.deleteTodo = function(index) {
+		$scope.loading = true;
+
+		var todo = $scope.todos[index];
+
+		$http.delete('/api/todos/' + todo.id)
+			.success(function() {
+				$scope.todos.splice(index, 1);
+					$scope.loading = false;
+
+			});;
+	};
+
+
+
+	$scope.init();
+
+});
+
+// app.directive('datepicker', function () {
+// 	console.log('here');
+// return {
+//     restrict: 'A',
+//     require: 'ngModel',
+//      link: function (scope, element, attrs, ngModelCtrl) {
+// 			 	console.log('here');
+//             $(element).datepicker({
+//                 dateFormat: ' yy,dd, MM',
+//                 onSelect: function (date) {
+// 								//	scope.date = date;
+//                   //  scope.$apply();
+//                 }
+//             });
+//         }
+//     };
+// });
---
+++ modules/Admin/views/payment/home.blade.php
@@ -0,0 +1,101 @@
+<div class="container" ng-app="paymentApp" ng-controller="paymentController">
+            <!-- END SIDEBAR -->
+            <!-- BEGIN CONTENT -->
+             <div class="page-content-wrapper">
+                <!-- BEGIN CONTENT BODY -->
+                <div class="page-content">
+                    <!-- BEGIN PAGE HEAD-->
+
+                    <!-- END PAGE HEAD-->
+                    <!-- BEGIN PAGE BREADCRUMB -->
+                   @include('packages::partials.breadcrumb')
+
+                    <!-- END PAGE BREADCRUMB -->
+                    <!-- BEGIN PAGE BASE CONTENT -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!-- BEGIN EXAMPLE TABLE PORTLET-->
+                            <div class="portlet light portlet-fit bordered">
+                                <div class="portlet-title">
+                                    <div class="caption">
+                                        <i class="icon-settings font-red"></i>
+                                        <span class="caption-subject font-red sbold uppercase">{{ $heading }}</span>
+                                    </div>
+                                </div>
+                                    @if(Session::has('flash_alert_notice'))
+                                         <div class="alert alert-success alert-dismissable" style="margin:10px">
+                                            <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+                                          <i class="icon fa fa-check"></i>
+                                         {{ Session::get('flash_alert_notice') }}
+                                         </div>
+                                    @endif
+                                <div class="portlet-body">
+                                    <table class="table table-striped table-hover table-bordered" id="contact">
+                                        <thead>
+                                            <tr>
+                                                 <th>Task Id</th>
+                                                <th>Task Title</th>
+                                                <th>Amount</th>
+                                                <th>Service Charge</th>
+                                                 <th>Net Amount</th>
+                                                <th>Doer Id</th>
+                                                <th>Doer Name</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                          <tr ng-repeat='todo in todos'>
+                                              <td><% todo.id %> </td>
+                                              <td><% todo.title %> </td>
+                                              <td><% todo.totalAmount %> </td>
+                                              <td><% todo.totalAmount*0.10 %> </td>
+                                              <td><% todo.totalAmount-todo.totalAmount*0.10 %> </td>
+                                              <td><% todo.taskDoerId %> </td>
+                                              <td>Mehul Ahir</td>
+                                              <td><button class="btn btn-primary btn-md"  ng-click="releaseFund(todo.id,todo.userId)">Release Fund</button></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <span>
+
+                                </div>
+                            </div>
+                            <!-- END EXAMPLE TABLE PORTLET-->
+                        </div>
+                    </div>
+                    <!-- END PAGE BASE CONTENT -->
+                </div>
+                <!-- END CONTENT BODY -->
+            </div>
+
+
+            <!-- END QUICK SIDEBAR -->
+        </div>
+
+
+     <div id="responsive" class="modal fade" tabindex="-1" data-width="300">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                <h4 class="modal-title">Contact Group</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4>Contact Group Name</h4>
+                        <p>
+                            <input type="text" class="col-md-12 form-control" name="contact_group" id="contact_group"> </p>
+                            <input type="hidden" name="contacts_id" value="">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+            <span id="error_msg"></span>
+                <button type="button" data-dismiss="modal" class="btn dark btn-outline">Close</button>
+                <button type="button" class="btn red" id="csave"  onclick="createGroup('{{url("admin/createGroup")}}','save')" >Save</button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
