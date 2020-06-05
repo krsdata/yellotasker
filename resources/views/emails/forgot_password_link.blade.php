5ed9faa0991e3d6ffa666b8c0f75d54ff230da90
krsdata <kroy.iips@gmail.com>
2020-06-06 03:08:58 +0530

    email template and filter api

--- resources/views/emails/forgot_password_link.blade.php
+++ resources/views/emails/forgot_password_link.blade.php
@@ -43,16 +43,40 @@
         <p style="color:#2a2c2d;font-family:MuseoSans-300,'Helvetica Neue',Helvetica,Arial,'Lucida Grande',sans-serif;font-size:16px;font-weight:300;line-height:125%;margin:24px 0;padding:0;text-align:Left" align="Left">
          <strong style="font-family:MuseoSans-500,'Helvetica Neue',Helvetica,Arial,'Lucida Grande',sans-serif;font-weight:500">Hi {{$content['name']}},</strong></p>
 <p style="color:#2a2c2d;font-family:MuseoSans-300,'Helvetica Neue',Helvetica,Arial,'Lucida Grande',sans-serif;font-size:16px;font-weight:300;line-height:125%;margin:24px 0;padding:0;text-align:Left" align="Left">
-           <p>Looks like you need to reset your password. Please click the link below on.</p>
-              <p>  
+           <p>You have used the ‘forgotten password’ option to reset your Yellotasker password. </p>
+              <p> Please use the following link so that we can verify your identity and reset your password: 
               </p>
-        <div class="m_6462014940457833709mobile-hide">
-         <a href="<?php echo 'http://yellotasker.com/#/update-password;token='.urlencode($content['temp_password']).';key='.urlencode($content['encrypt_key']); ?>" style="background:#efeb10; color:#000; padding:10px 25px; float:right; text-decoration:none; border-radius:40px;">Reset Password</a>
 
-     <div>
+        <div class="m_6462014940457833709mobile-hide">
+         <a href="<?php echo 'http://yellotasker.com/#/update-password;token='.urlencode($content['temp_password']).';key='.urlencode($content['encrypt_key']); ?>" style="background:#efeb10; color:#000; padding:10px 25px; float:left; text-decoration:none; border-radius:40px;">Reset Password</a>
+       </div>
+
 </td>
 </tr>
-</tbody></table>
+<tr>
+  <td>
+     <div>
+          <p>The link is valid for 48 hours and can be used only once. Once the link is expired, you will need to restart the password reset process by using the ‘forgotten password’ option from the login screen. </p>
+        </div>
+  </td>
+</tr>
+<tr>
+  <td>
+    <br><br>
+    Yours Sincerely, <br><br>
+
+    <b>Yellotasker</b>
+  </td>
+</tr>
+<tr>
+  <td><br><br>
+    <b>Important Notice</b>: The content of this email is intended only for use by the individual or entity to whom it is addressed. If you have received this email by mistake, please advise the sender and delete the message and attachments immediately.  This email, including attachments, may contain confidential, sensitive, legally privileged and/or copyright information.
+
+  </td>
+</tr>
+</tbody>
+
+</table>
 
 </td>
 </tr>
@@ -64,38 +88,15 @@
     <table border="0" cellpadding="0" cellspacing="0" class="m_6462014940457833709flexibleContainer" style="border-collapse:collapse" width="552">
      
       <tbody><tr>
-       <td align="center" class="m_6462014940457833709flexibleContainerCell" style="padding-left:32px;padding-right:32px;padding-top:20px" valign="top" width="552">
+       <td align="center" class="m_6462014940457833709flexibleContainerCell" style="padding-left:32px;padding-right:32px;padding-top:0px" valign="top" width="552">
         <table border="0" cellpadding="0" cellspacing="0" style="border-collapse:collapse" width="100%">
          
           <tbody><tr>
            <td class="m_6462014940457833709textContent" style="color:#2a2c2d;font-family:MuseoSans-300,'Helvetica Neue',Helvetica,Arial,'Lucida Grande',sans-serif;font-size:16px;line-height:125%;padding-bottom:20px;text-align:Left" valign="top" align="Left">
             <table border="0" cellpadding="0" cellspacing="0" class="m_6462014940457833709offer-task-block" style="border-collapse:collapse;margin:16px 0" width="100%">
              
-              <tbody><tr>
-               <td class="m_6462014940457833709mobile-block m_6462014940457833709mobile-text-center" width="64px">
-                <img height="48" src="{{url('storage/image/icon-y.png')}}" style="border:0;outline:none;text-decoration:none" width="48" class="CToWUd">
-</td>
-<td class="m_6462014940457833709mobile-70-block">
-                <p class="m_6462014940457833709margin-4-0" style="color:#2a2c2d;font-family:MuseoSans-300,'Helvetica Neue',Helvetica,Arial,'Lucida Grande',sans-serif;font-size:16px;font-weight:300;line-height:14px;margin:4px 0;padding:0;text-align:Left" align="Left">
-                 <a href="#" style="color:#000; text-decoration:none">Yellotasker support C.</a></p>
-<p class="m_6462014940457833709margin-4-0" style="color:#2a2c2d;font-family:MuseoSans-300,'Helvetica Neue',Helvetica,Arial,'Lucida Grande',sans-serif;font-size:16px;font-weight:300;line-height:14px;margin:4px 0;padding:0;text-align:Left" align="Left">
-                 <img class="m_6462014940457833709inline-block CToWUd" height="" src="{{url('storage/image/rating.png')}}" width="100" style="border:0;outline:none;text-decoration:none"></p>
-
-</td>
-<td class="m_6462014940457833709mobile-30-block">
-                <p class="m_6462014940457833709price m_6462014940457833709text-center m_6462014940457833709mobile-text-right" style="color:#2a2c2d;font-family:MuseoSans-500,'Helvetica Neue',Helvetica,Arial,'Lucida Grande',sans-serif;font-size:28px;font-weight:500;letter-spacing:-.29px;line-height:28px;margin:24px 0;padding:0;text-align:center;vertical-align:middle" align="center">
-                 </p>
-</td>
-</tr>
+              <tbody>
 <tr>
-               <td class="m_6462014940457833709mobile-block m_6462014940457833709description m_6462014940457833709mobile-16-font" colspan="2">
-                <p style="color:#2a2c2d;font-family:MuseoSans-300,'Helvetica Neue',Helvetica,Arial,'Lucida Grande',sans-serif;font-size:14px;font-weight:300;line-height:20px;margin:16px 0;padding:0;text-align:Left" align="Left">
-                <br>
-                 Yellotasker Team</p>
-</td>
-<td class="m_6462014940457833709cta m_6462014940457833709mobile-block" style="padding-top:16px;vertical-align:top" width="107" valign="top">
-                
-</td>
 </tr>
 </tbody></table>
 </td>
